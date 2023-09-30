35c5e3c62b903a5f9a50fb5a30e824ee62838726
ashwinidevelops <126318913+ashwinidevelops@users.noreply.github.com>
2023-10-01 00:19:58 +0530

    Update CountingSort.java

--- Arrays/CountingSort.java
+++ Arrays/CountingSort.java
@@ -39,3 +39,18 @@ public class CountingSort {
  *   Output:
  *   1 1 2 3 3 4 4 7
  */
+/*
+Initialization: The code starts by initializing a variable largest to Integer.MIN_VALUE, which will be used to find the largest element in the input array.
+
+Find the Largest Element: A loop is used to iterate through the input array, and the largest variable is updated to store the largest element encountered in the array.
+
+Counting Array: An array called count is created with a size of largest + 1. This array will be used to count the occurrences of each element in the input array.
+
+Counting Occurrences: Another loop is used to iterate through the input array again. For each element array[i], the corresponding index in the count array is incremented. This step counts the occurrences of each unique element in the input array.
+
+Sorting: After counting the occurrences of each element, the code uses a while loop to iterate through the count array. For each non-zero count at index i, it updates the original array with the value i and decrements the count at index i. This effectively places each element back into the original array in sorted order.
+
+Printing the Sorted Array: Finally, in the main method, the sortArray function is called to sort the array and then the sorted elements are printed to the console.
+
+Output: The sorted array is printed to the console, showing the elements in ascending order.
+    */
